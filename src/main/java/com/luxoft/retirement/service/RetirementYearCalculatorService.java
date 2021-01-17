011e8cc04c694a92c00b0be5aa7cfd3d7b9dae0e
Mahmud Khudobergaov <mahmudk498@gmail.com>
2021-01-17 14:22:45 +0600

    Fix/use date time api (#1)
    * Use Date Time API
    
    * Use Date Time API

--- src/main/java/com/luxoft/retirement/service/RetirementYearCalculatorService.java
+++ src/main/java/com/luxoft/retirement/service/RetirementYearCalculatorService.java
@@ -1,17 +1,16 @@
 package com.luxoft.retirement.service;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Calendar;
+import java.time.LocalDate;
 
 @Service
 @AllArgsConstructor
 public class RetirementYearCalculatorService {
 
   public int calculateYear(int ageLeft) {
-    return Calendar.getInstance().get(Calendar.YEAR) + ageLeft;
+    return LocalDate.now().getYear() + ageLeft;
   }
 
 }
